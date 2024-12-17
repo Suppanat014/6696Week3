0ca14f9217ace084bb1e002deac6f9adf5be934c
Peet <peet.08.spn@gmail.com>
2024-12-17 10:44:00 +0700

    Ex1

---
+++ Ex2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <!-- เพิ่ม font -->
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+   <style>
+       body{
+        font-family: "Chakra Petch", sans-serif;
+        font-weight: 300;
+        font-style: italic
+     }
+   </style>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ตัวอย่าง1</title>
+</head>
+<body>
+    <h1>โปรเเกรม ภาษา HTML</h1>
+    664485014 นายศุภณัฐ ตามใจเพียร <br>
+    66/96 
+</body>
+</html>
